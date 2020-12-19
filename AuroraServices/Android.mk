b080ebddf33815716081190f414927a33d6ce045
Lukas Rieder <1394536+lephyrus@users.noreply.github.com>
2020-12-19 17:53:39 +0100

    Upgrade Store and Services apps

--- AuroraServices/Android.mk
+++ AuroraServices/Android.mk
@@ -6,7 +6,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := privapp-permissions-AuroraServices.xml
+LOCAL_MODULE := permissions_com.aurora.services.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
@@ -16,11 +16,11 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := AuroraServices
-LOCAL_SRC_FILES := AuroraServices-v1.0.4.apk
+LOCAL_SRC_FILES := AuroraServices-v1.0.6.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REQUIRED_MODULES := privapp-permissions-AuroraServices.xml
+LOCAL_REQUIRED_MODULES := permissions_com.aurora.services.xml
 include $(BUILD_PREBUILT)
 
