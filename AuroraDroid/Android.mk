152d5b3b0d949f062f06ca5cecc5b3f583f6c84d
Pinembour <pinembour@teppelin.fr>
2021-03-03 12:17:23 +0100

    Add AuroraDroid prebuilt

---
+++ AuroraDroid/Android.mk
@@ -0,0 +1,16 @@
+
+#
+# AuroraStore as privileged app as part of a build
+#
+# Prebuilt content taken from https://gitlab.com/AuroraOSS/AuroraStore/tags
+#
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := AuroraDroid
+LOCAL_SRC_FILES := AuroraDroid_1.0.7.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
